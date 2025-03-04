15db78734c2fe204567ede25a652a8c39816ef2a
izzathabib <izzathabib680@gmail.com>
2025-03-04 12:05:48 +0800

    Enhance sidebar responsiveness with mobile layout adjustments and toggle functionality

--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggle-btn");
   const sidebar = document.getElementById("sidebar");
   const dashboardText = document.getElementById("dashboard-text");
-  const bottomNav = document.getElementById("bottomNav");
   const menuTexts = document.querySelectorAll(".menu-text");
   const eventManagementSubmenu = document.getElementById("event-management-submenu");
   const sidebarDropdownIcon = document.getElementById("sidebar-dropdown-menu"); // Dropdown icon only
   const eventManagementToggle = document.getElementById("event-management-toggle"); // Event management menu including icon + dropwdown toogle
+  const notiProfile = document.getElementById("noti-profile"); 
+  const mobileTopNav = document.getElementById("mobile-top-nav"); 
+  const mobileSidebarButton = document.getElementById("mobile-sidebar-button");
+  const mainSection = document.getElementById("main-section");
+  const xIcon = document.getElementById("x-icon");
+
 
   function hideEventManagementSubmenu() {
     if (eventManagementSubmenu.classList.contains("hidden")) {
@@ -16,39 +21,60 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function mobileScreenLayout() {
+    sidebar.classList.add("hidden");
+    notiProfile.classList.add("hidden");
+    mobileTopNav.classList.remove("hidden");
+  }
+
+  function tabletScreenLayout() {
+    sidebar.classList.remove("hidden");
+    notiProfile.classList.remove("hidden");
+    sidebar.classList.add("w-16");
+    sidebar.classList.remove("w-64");
+    dashboardText.classList.add("hidden");
+    menuTexts.forEach(text => text.classList.add("hidden"));
+    sidebarDropdownIcon.classList.toggle("hidden");
+    mobileTopNav.classList.add("hidden");
+  }
+
+  function largeScreenLayout() {
+    sidebar.classList.remove("hidden");
+    notiProfile.classList.remove("hidden");
+    sidebar.classList.remove("w-16");
+    sidebar.classList.add("w-64");
+    dashboardText.classList.remove("hidden");
+    menuTexts.forEach(text => text.classList.remove("hidden"));
+    sidebarDropdownIcon.classList.remove("hidden");
+    mobileTopNav.classList.add("hidden");
+  }
+
   // Sidebar behavior based on screen size 
   function responsiveSidebar() {
     const width = window.innerWidth;
     switch (true) {
       case (width <= 640):
-        sidebar.classList.add("hidden");
-        bottomNav.classList.remove("hidden");
+        mobileScreenLayout();
         break;
       case (width <= 1024 && width > 640):
-        bottomNav.classList.add("hidden");
-        sidebar.classList.remove("hidden");
-        sidebar.classList.add("w-16");
-        sidebar.classList.remove("w-64");
-        dashboardText.classList.add("hidden");
-        menuTexts.forEach(text => text.classList.add("hidden"));
-        sidebarDropdownIcon.classList.toggle("hidden");
-        hideEventManagementSubmenu()
+        tabletScreenLayout();
+        hideEventManagementSubmenu();
         break;
       default:
-        bottomNav.classList. add("hidden");
-        sidebar.classList.remove("hidden");
-        sidebar.classList.remove("w-16");
-        sidebar.classList.add("w-64");
-        dashboardText.classList.remove("hidden");
-        menuTexts.forEach(text => text.classList.remove("hidden"));
-        sidebarDropdownIcon.classList.toggle("hidden");
+        largeScreenLayout();
         break;
     }
+  }
+
+  // Hide sidebar on page load if screen width is less than or equal to 640px
+  if (window.innerWidth <= 640) {
+    mobileScreenLayout();
   }
 
   // Resizes sidebar
   window.addEventListener("resize", responsiveSidebar);
 
+  // Normal screen sidebar toggle button 
   toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("w-16");
     dashboardText.classList.toggle("hidden");
@@ -57,6 +83,26 @@ document.addEventListener("DOMContentLoaded", () => {
     sidebar.classList.toggle("w-64");
     eventManagementSubmenu.classList.toggle("hidden"); 
     hideEventManagementSubmenu()
+  });
+
+  // Mobile screen sidebar toggle button
+  mobileSidebarButton.addEventListener("click", () => {
+    sidebar.classList.remove("hidden");
+    sidebar.classList.add("w-full");
+    sidebar.classList.remove("w-64");
+    mainSection.classList.add("hidden");
+    toggleBtn.classList.add("hidden");
+    xIcon.classList.remove("hidden");
+  });
+
+  // "X" icon to close mobile sidebar
+  xIcon.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+    sidebar.classList.toggle("w-full");
+    sidebar.classList.add("w-64");
+    mainSection.classList.remove("hidden");
+    toggleBtn.classList.remove("hidden");
+    xIcon.classList.add("hidden");
   });
 
   // Event management dropdown menu button
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,14 @@
                     </svg>
                 </span>
             </button>
+            <button id="x-icon" class="hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
             <span class="text-black text-lg font-extrabold" id="dashboard-text">EventUSM</span>
         </div>
-        <nav class="mt-3">
+        <nav id="sidebar-menu" class="mt-3">
             <ul>
                 <li class="{{ Route::is('dashboard')? 'active' : '' }} rounded-md p-3 hover:bg-slate-300 cursor-pointer">
                     <a href="{{ route('dashboard') }}" class="flex items-center gap-4">
@@ -62,12 +67,12 @@
                         </span>
                     </button>
                     <ul id="event-management-submenu" class="mt-2 hidden">
-                        <li class="{{ Route::is('event_management.application') ? 'border-purple-600 font-semibold' : '' }} ml-9 p-2 border-l-4 border-purple-300 hover:border-purple-600 hover:font-semibold">
+                        <li class="{{ Route::is('event_management.application') ? 'border-purple-600 font-semibold' : '' }} ml-12 p-2 border-l-2 border-purple-300 hover:border-purple-600 hover:font-semibold">
                             <a href="{{ route('event_management.application') }}" class="cursor-pointer">
                                 <span class="text-sm">Application</span>
                             </a>
                         </li>
-                        <li class="ml-9 p-2 cursor-pointer border-l-4 border-purple-300 hover:border-purple-600 hover:font-semibold">
+                        <li class="ml-12 p-2 cursor-pointer border-l-2 border-purple-300 hover:border-purple-600 hover:font-semibold">
                             <a href="#" class="">
                                 <span class="text-sm">Success Report</span>
                             </a>
@@ -78,8 +83,24 @@
         </nav>
     </aside>
     <!-- Main Content -->  
-    <main class="flex-1 p-6">
-        <div class="flex flex-wrap gap-7 mb-4 justify-end">
+    <main id="main-section" class="flex-1 flex-col p-6">
+        <!-- Top navigation for mobile screen -->
+        <div id="mobile-top-nav" class="flex justify-between hidden mb-4">
+            <button id="mobile-sidebar-button">
+                <span id="toggle-icon" class="text-black">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </span>
+            </button>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                </svg>
+            </div>
+        </div>
+        <!-- Notification and User Profile -->
+        <div id="noti-profile" class="flex flex-wrap gap-7 mb-4 justify-end">
             <!-- Notification Icon -->
             <div class="flex">
                 <div class="w-9 h-9 rounded-full bg-purple-600 flex items-center justify-center text-white">
@@ -102,23 +123,5 @@
         </div>
         @yield('content')
     </main>
-    <!-- Bottom navigation for phone user -->
-    <div id="bottomNav" class="bottomNav hidden">
-        <a href="#dashboard" class="active">
-            <span class="text-black">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-                </svg>
-            </span>
-        </a>
-        <a href="#home">
-            <span class="text-black">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-            </span>
-        </a>
-        <a href="#contact">Contact</a>
-    </div>
 </body>
 </html>
