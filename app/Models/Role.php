46d65120af9c7224688161067d0109b37335aeb3
izzathabib <izzathabib680@gmail.com>
2025-03-28 16:26:28 +0800

    Add Role model and establish relationship with User model

---
+++ app/Models/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Role extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['description']; // Add other fillable attributes if needed
+
+    /**
+     * Get the users for the role.
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+}
