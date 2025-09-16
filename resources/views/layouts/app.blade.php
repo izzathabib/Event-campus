1099d49062ba6c97d7bdc4075541dd438d92dab1
izzathabib <izzathabib680@gmail.com>
2025-09-16 23:10:55 +0800

    Change hover color on event management

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
                     </li>
                     @if (Auth::user()->roles->desc == 'society')
                     <li class="">
-                        <button id="event-management-toggle" class="{{ Route::is(['society.event_application_view', 'society.create-event-view'])? 'text-purple-400 font-semibold':'' }} cursor-pointer flex items-center gap-4 hover:bg-slate-300 hover:text-purple-900 p-3 rounded-md w-full">
+                        <button id="event-management-toggle" class="{{ Route::is(['society.event_application_view', 'society.create-event-view'])? 'text-purple-300 font-semibold':'' }} cursor-pointer flex items-center gap-4 hover:bg-slate-300 hover:text-purple-900 p-3 rounded-md w-full">
                             <span class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
