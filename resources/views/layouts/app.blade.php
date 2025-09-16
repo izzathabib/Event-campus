94b4189ece39bba806ee13cab74aff9f8ed0326a
izzathabib <izzathabib680@gmail.com>
2025-09-16 22:37:42 +0800

    Comment out notification icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -156,14 +156,14 @@
         <!-- Notification and User Profile -->
         <div id="noti-profile" class="flex flex-wrap gap-7 mb-4 justify-end">
             <!-- Notification Icon -->
-            <div class="flex">
+            <!-- <div class="flex">
                 <div class="w-9 h-9 rounded-full bg-purple-900 flex items-center justify-center text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
                 </div>
                 <span class="relative top-5 right-2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">3</span>
-            </div>
+            </div> -->
             <!-- User Profile -->
             <div class="flex items-center">
                 <div class="mr-3">
